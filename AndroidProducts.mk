494d763251fc45bf0b916b9d90e0c6481e74959b
4mede <4mede@tutamail.com>
2024-07-27 14:25:34 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spaced.mk
+    $(LOCAL_DIR)/lineage_x6827.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spaced-user \
-    lineage_spaced-userdebug \
-    lineage_spaced-eng
+    lineage_x6827-user \
+    lineage_x6827-userdebug \
+    lineage_x6827-eng
